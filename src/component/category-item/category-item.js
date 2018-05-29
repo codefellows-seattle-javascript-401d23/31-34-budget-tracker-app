9451d42dd3783a030a9defb77b842f4f835c6f43
Sarah Geyer <sjgeyer@comcast.net>
2018-05-29 16:56:39 -0700

    change update form to modal

--- src/component/category-item/category-item.js
+++ src/component/category-item/category-item.js
@@ -2,19 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CategoryForm from '../category-form/category-form';
+import Modal from '../modal/modal';
 import * as categoryActions from '../../action/category';
 import './category-item.scss';
 
 class CategoryItem extends React.Component {
   render() {
     const { category, categoryUpdate, categoryDestroy } = this.props;
+
+    const showModal = () => categoryUpdate({ ...category, editing: true });
+    const hideModal = () => categoryUpdate({ ...category, editing: false });
+    // const updateAndClose = (updatedNote) => {
+    //   handleUpdateNote({ ...updatedNote, id: note.id, editing: false });
+    // };
+
     return (
       <div className='category-item'>
         <h4>{category.name.toUpperCase()}</h4>
         <p>Budget allotted: ${category.budget}</p>
         <p>Description: {category.description}</p>
         <button onClick={() => categoryDestroy(category)}> remove </button>
-        <CategoryForm onComplete={categoryUpdate} category={category}/>
+        <button onClick={showModal}> update </button>
+        <Modal show={category.editing} handleClose={hideModal}>
+          <CategoryForm onComplete={categoryUpdate} category={category}/>
+        </Modal>
       </div>
     );
   }
