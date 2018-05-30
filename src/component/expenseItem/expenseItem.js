46399e8e26d37dd07ecaab0507ee8bda83da28d4
Josiah Green <JosiahGreen90@gmail.com>
2018-05-30 13:59:42 -0700

    working on combined reducer

---
+++ src/component/expenseItem/expenseItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ExpenseForm from './../expenseForm/expenseForm';
+import * as expenseActions from '../../action/expense';
+
+class Expense extends React.Component {
+  render() {
+    const { expense, expenseDestroy, expenseUpdate } = this.props;
+    return (
+    <div className='expense'>
+      <p> { expense.content } </p>
+      <button onClick={() => expenseDestroy(expense)}>Delete</button>
+      <ExpenseForm
+        expense={expense}
+        onComplete={expenseUpdate}
+      />
+    </div>
+    );
+  }
+}
+
+Expense.propTypes = {
+  expense: PropTypes.object,
+  expenseDestroy: PropTypes.func,
+  expenseUpdate: PropTypes.func,
+};
+
+const mapDispatchToProps = dispatch => ({
+  expenseDestroy: data => dispatch(expenseActions.removeAction(data)),
+  expenseUpdate: data => dispatch(expenseActions.updateAction(data)),
+});
+
+export default connect(null, mapDispatchToProps)(Expense);
