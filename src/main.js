f2e9cde56a3199672418d0a5e4e6120277a4d82b
Sarah Geyer <sjgeyer@comcast.net>
2018-05-30 16:21:01 -0700

    add lib/store.js and edit README

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render as renderDOM } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './component/app/app';
-import reducer from './reducer/index';
-
-const middleware = {};
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
+import store from './lib/store';
 
 const app = document.createElement('div');
 document.body.appendChild(app);
